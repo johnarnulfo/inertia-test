fdf84d7d76d51784d692e7620652a6ba63351d79
John Arnulfo Navera <john.arnulfo@foxholeqa.com>
2022-10-27 07:47:49 +0800

    first

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap'
+
+import '../css/app.css'
+
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+createInertiaApp({
+  resolve: name => import(`./pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-white p-10 m-10 rounded border">
+        <h1>Hello World</h1>
+        <p>{{ name }}</p>
+
+        <input type="text" name="name" id="name" class="w-full border rounded p-2">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: String,
+        },
+    };
+</script>
